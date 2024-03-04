baee493e525adf65c86fc2349be68cbc1b527cf6
Pavel Mezencev <mezencev24@mail.ru>
2024-03-04 16:36:30 +0300

    add useTheme

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+import { FC, useMemo, useState } from 'react';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+
+const defaultProps =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+
+export const ThemeProvider: FC = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultProps);
+
+  const defaultValue = useMemo(
+    () => ({
+      theme,
+      setTheme,
+    }),
+    [theme]
+  );
+
+  return (
+    <ThemeContext.Provider value={defaultValue}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
